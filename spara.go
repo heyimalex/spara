55c90eb2964dee5ba3324c9f2b493df927c47828
Alex Guerra <alex@heyimalex.com>
2016-11-04 13:32:36 -0500

    Use context.Context instead of custom context

--- spara.go
+++ spara.go
@@ -3,6 +3,7 @@
 package spara
 
 import (
+	"context"
 	"errors"
 	"sync"
 	"sync/atomic"
@@ -14,13 +15,13 @@ var (
 )
 
 func Run(workers int, iterations int, fn func(int) error) error {
-	wrapped := func(_ Context, idx int) error {
+	wrapped := func(_ context.Context, idx int) error {
 		return fn(idx)
 	}
 	return RunWithContext(workers, iterations, wrapped)
 }
 
-func RunWithContext(workers int, iterations int, fn func(Context, int) error) error {
+func RunWithContext(workers int, iterations int, fn func(context.Context, int) error) error {
 
 	if workers <= 0 {
 		return ErrInvalidWorkers
@@ -47,7 +48,17 @@ func RunWithContext(workers int, iterations int, fn func(Context, int) error) er
 		return int(atomic.AddInt32(&index, 1))
 	}
 
-	ctx := &context{cancelchan: make(chan struct{})}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var firsterr error
+	var killed int64
+	kill := func(err error) {
+		if atomic.CompareAndSwapInt64(&killed, 0, 1) {
+			cancel()
+			firsterr = err
+		}
+	}
 
 	var wg sync.WaitGroup
 	wg.Add(workers)
@@ -56,11 +67,11 @@ func RunWithContext(workers int, iterations int, fn func(Context, int) error) er
 		go func() {
 			defer wg.Done()
 			for j := nextIndex(); j < iterations; j = nextIndex() {
-				if ctx.IsCanceled() {
+				if atomic.LoadInt64(&killed) == 1 {
 					return
 				}
 				if err := fn(ctx, j); err != nil {
-					ctx.cancel(err)
+					kill(err)
 					return
 				}
 			}
@@ -68,45 +79,5 @@ func RunWithContext(workers int, iterations int, fn func(Context, int) error) er
 	}
 
 	wg.Wait()
-	return ctx.err
-}
-
-// Context exposes information about early cancellation to the work function. This allows work
-// functions that are long or have multiple stages to avoid doing unnecessary work by returning
-// early when the process has been canceled.
-//
-// Imagine a work function that downloads files. If any of the downloads fail, no more downloads
-// will be initiated, but all of the currently in-progress downloads will continue to run until
-// completion. Since the `Run` functions don't return until all workers have completed, you'll be
-// waiting on a bunch of requests to complete that you really don't care about.
-//
-// A better way to handle this would be using Context. When you create the `net/http.Request` to
-// send, you can set its `Cancel` field to `Context.Canceled()`. Then whenever the process
-// encounters an error, any in progress downloads will be terminated immediately.
-type Context interface {
-	// Returns whether or not the process has encountered an error.
-	IsCanceled() bool
-	// Returns a channel that will be closed when the process has encountered an error.
-	Canceled() <-chan struct{}
-}
-
-type context struct {
-	cancelswitch uint32
-	cancelchan   chan struct{}
-	err          error
-}
-
-func (ctx *context) cancel(err error) {
-	if atomic.CompareAndSwapUint32(&ctx.cancelswitch, 0, 1) {
-		ctx.err = err
-		close(ctx.cancelchan)
-	}
-}
-
-func (ctx *context) IsCanceled() bool {
-	return atomic.LoadUint32(&ctx.cancelswitch) == 1
-}
-
-func (ctx *context) Canceled() <-chan struct{} {
-	return ctx.cancelchan
+	return firsterr
 }
