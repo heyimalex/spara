0857793685f614d71890e33a784c77155dbface1
Alex Guerra <alex@heyimalex.com>
2016-11-28 16:46:01 -0600

    Remove backticks from docstrings
    They're not rendered on godoc.org like they are in markdown, so it's
    just visual noise

--- spara.go
+++ spara.go
@@ -16,14 +16,14 @@ var (
 )
 
 // Run takes a mapping function and then runs it concurrently across up to
-// `workers` separate goroutines, calling it with every number in the range
-// `[0, iterations-1]`. If the mapping function returns an error, iteration
-// will stop prematurely and Run will return that error once all outstanding
-// calls to the mapping function complete.
+// workers separate goroutines, calling it with every number in the range [0,
+// iterations-1]. If the mapping function returns an error, iteration will
+// stop prematurely and Run will return that error once all outstanding calls
+// to the mapping function complete.
 //
 // In more human terms, Run is just an interesting way to write a concurrent
-// version of the higher order function, `map`. Because go doesn't have
-// generics, we can't really write `map` without a bunch of type assertions.
+// version of the higher order function, map. Because go doesn't have
+// generics, we can't really write map without a bunch of type assertions.
 // But, if instead of passing each value of the array we passed each _index_,
 // we can recover some of the convenience of map with just a little more
 // boilerplate.
@@ -40,19 +40,19 @@ type MappingFunc func(ctx context.Context, index int) error
 // RunWithContext is exactly like Run except that it:
 //
 // - Accepts a parent context. Completion of this context is tracked via it's
-//   `Done()` channel and will stop iteration early if possible. If completion
-//   _does_ cause iteration to stop, the error returned from RunWithContext will
-//   be the value of `parent.Err()`. The passed context may be nil, in which
-//   case it defaults to `context.Background()`.
+//   Done() channel and will stop iteration early if possible. If completion
+//   _does_ cause iteration to stop, the error returned from RunWithContext
+//   will be the value of parent.Err(). The passed context may be nil, in
+//   which case it defaults to context.Background().
 // - Passes a context to the mapping function. This context will be a child of
 //   the provided parent context, and will complete either on the first
 //   returned error, the parent completing, or all of the work being done.
 //
 // This method can give very large performance improvements when elements of
 // the mapping function support context for early cancallation (eg
-// http.Request's `WithContext`). Imagine a function that downloads multiple
-// files in parallel. Using Run, the first error would cause no _new_ files
-// to begin downloading, but it still would not return until all of the in
+// http.Request's WithContext). Imagine a function that downloads multiple
+// files in parallel. Using Run, the first error would cause no _new_ files to
+// begin downloading, but it still would not return until all of the in
 // progress downloads completed. If the in progress downloads are large, that
 // could mean you're waiting a very long time for a bunch of data you don't
 // actually care about. With early cancellation, these requests would be
